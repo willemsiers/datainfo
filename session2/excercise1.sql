fbb908cfb756878577f10d36325d6c4fe1c3635a
willemsiers <willemsiers@github.com>
2014-05-06 14:55:39 +0200

    session2 first commit

---
+++ session2/excercise1.sql
@@ -0,0 +1,2 @@
+CREATE TABLE Problem1 (
+task CHAR(1) PRIMARY KEY, result BOOLEAN, motivation VARCHAR(200));
---
+++ session2/problem1.sql
@@ -0,0 +1,31 @@
+INSERT INTO Problem1 (task, result, motivation) 
+VALUES("A", 0, "meerdere Voertuigen kunnen dezelfde Capaciteit hebben");
+
+INSERT INTO Problem1 (task, result, motivation) 
+VALUES("B", 1, "ieder Voertuig heeft maar één Capaciteit.");
+
+INSERT INTO Problem1 (task, result, motivation) 
+VALUES("C", 1, "want (c), dus ieder pakje is uniek voor een Factuur.");
+
+INSERT INTO Problem1 (task, result, motivation) 
+VALUES("D", 1, "want uit c volgt dat uit elk pakje een factuur kan worden afgeleid, en uit a volgt de ontvanger.");
+
+INSERT INTO Problem1 (task, result, motivation) 
+VALUES("E", 1, "uit pakje volgt prijs (5) & uit pakje volg ontvanger (4).");
+
+INSERT INTO Problem1 (task, result, motivation) 
+VALUES("F", 0, "Bezorger kan meerdere voertuigen gebruiken");
+
+INSERT INTO Problem1 (task, result, motivation) 
+VALUES("G", 0, "Voertuig kan door meerderen worden bestuurd");
+
+INSERT INTO Problem1 (task, result, motivation) 
+VALUES("H", 0, "Bezorger kan meerdere voertuigen gebruiken");
+
+INSERT INTO Problem1 (task, result, motivation) 
+VALUES("I", 0, "Bezorger kan meerdere voertuigen gebruiken");
+
+
+INSERT INTO Problem1 (task, result, motivation) 
+VALUES("J", 0, "Bezorger kan meerdere voertuigen gebruiken");
+
