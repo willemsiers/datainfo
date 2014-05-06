48b820cdec4fffa6988522d3c8b874d6bca97d62
willemsiers <willemsiers@github.com>
2014-05-06 16:26:12 +0200

    session2 exc2(3)

--- session2/result2.sql
+++ session2/result2.sql
@@ -1,3 +1,3 @@
 INSERT INTO F1(x, y, violation, motivation) VALUES ("ABC", "EF", 0, "Kunt alle attributen krijgen");
-INSERT INTO F1(x, y, violation, motivation) VALUES ("D", "A", 1, "Kunt NIET alle attributen krijgen");
+INSERT INTO F1(x, y, violation, motivation) VALUES ("D", "A", 0, "Kunt wel alle attributen krijgen");
 INSERT INTO F2(x, y, violation, motivation) VALUES ("E", "D", 0, "Kunt alle attributen krijgen");
